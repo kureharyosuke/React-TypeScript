ce1f38fd5ae84c22003b5dad35c5e3b7439079ad
kureharyosuke <ohhayo.com@gmail.com>
2021-01-04 04:28:58 +0900

    #6. webpack & tsconfig : Setting

---
+++ lecture/client.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import GuGuDanClass from "./GuGuDanClass";
+
+ReactDOM.render(<GuGuDanClass />, document.querySelector("#root"));
