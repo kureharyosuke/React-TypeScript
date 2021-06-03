d355fbd0c0e006c6fc8a8899dcdb091da8aab049
kureharyosuke <ohhayo.com@gmail.com>
2021-06-04 00:26:18 +0900

    9. New ReactComponent Type - Good

--- React-Hook-Master/ts-hooks/src/EvenMoreReactComponent.tsx
+++ React-Hook-Master/ts-hooks/src/EvenMoreReactComponent.tsx
@@ -1,14 +1,12 @@
 
 export interface HeadingProps { title: string }
 
-export function Heading({ title }: { title: string }) {
+export function Heading({ title }: HeadingProps) {
   return (
     <h1>{title}</h1>
   )
 }
 
 function TestComponent() {
-  return (
-    <Heading title="Hello"
-  )
+  return <Heading title="Hello"
 }
