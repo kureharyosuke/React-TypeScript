d9ecbf5af152df2155e4e70bb9ce6a873dc8f74c
kureharyosuke <ohhayo.com@gmail.com>
2021-06-03 21:23:50 +0900

    1. useState

---
+++ React-Hook-Master/ts-hooks/src/UseStateComponent.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+
+function UseStateComponent() {
+
+  // const [arr, arrSet] = useState([]) // never[] | const arr: never[] | useState<never[]>(initialState: never[] | (() => never[])): [never[], React.Dispatch<React.SetStateAction<never[]>>] (+1 overload)
+
+  const [arr, arrSet] = useState<number[]>([])
+
+  // const [name, nameSet] = useState(null); // onst name: null
+
+  const [name, nameSet] = useState<string | null>(null);
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => arrSet([...arr, arr.length + 1])}>
+          Add to array
+        </button>
+        {JSON.stringify(arr)}
+      </div>
+      <div>
+        <button onClick={() => nameSet('jack')}>
+          Set  Name
+        </button>
+        {JSON.stringify(name)}
+      </div>
+    </div>
+  )
+}
+
+export default UseStateComponent
