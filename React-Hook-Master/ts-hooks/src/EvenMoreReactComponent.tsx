19c5c828831f85a6f80652025b719b815fdc8513
kureharyosuke <ohhayo.com@gmail.com>
2021-06-04 00:46:49 +0900

    10. New ReactComponent Type

--- React-Hook-Master/ts-hooks/src/EvenMoreReactComponent.tsx
+++ React-Hook-Master/ts-hooks/src/EvenMoreReactComponent.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from 'react'
 
 export interface HeadingProps { title: string }
 
@@ -11,11 +12,53 @@ export const Heading2 = ({ title }: HeadingProps) => {
   return <h2>{title}</h2>
 }
 
+export function List<ListItem>({
+  items,
+  render,
+}: {
+  items: ListItem[];
+  render: (item: ListItem) => ReactNode;
+}) {
+  return (
+    <ul>
+      {items.map((item, index) => (
+        <li key={index}>
+          {render(item)}
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export type ListComponent = <ListItem>({
+  items,
+  render,
+}: {
+  items: ListItem[];
+  render: (item: ListItem) => ReactNode;
+}) => ReactNode;
+
+export const List2: ListComponent = ({ items, render }) => {
+  return (
+    <ul>
+      {
+        items.map((item, index) => (
+          <li key={index}>
+            {render(item)}
+          </li>
+        ))
+      }
+    </ul >
+  )
+}
+
 function TestComponent() {
   return (
     <>
       <Heading title="Good" />
       <Heading2 title="Not Bad" />
+      <List items={["a", "b", "c"]} render={(str) => <strong>{str}</strong>} />
+      <List2 items={["a", "b", "c"]} render={(str) => <strong>{str}</strong>} />
     </>
   )
 }
