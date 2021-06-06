53aa6ca74739208b999a8704ca0cc7961e5608fa
kureharyosuke <ohhayo.com@gmail.com>
2021-06-06 19:25:41 +0900

    useState, props

--- React-Hook-Master/todo-base/src/components/TopBar.tsx
+++ React-Hook-Master/todo-base/src/components/TopBar.tsx
@@ -6,7 +6,7 @@ import { ColorModeSwitcher } from "./ColorModeSwitcher";
 JSON source: https://raw.githubusercontent.com/jherr/todos-four-ways/master/data/todos.json
 */
 
-function TopBar() {
+function TopBar({ todosSet }: { todosSet }) {
   return (
     <Grid pt={2} templateColumns="1fr 1fr" columnGap="3">
       <ColorModeSwitcher />
