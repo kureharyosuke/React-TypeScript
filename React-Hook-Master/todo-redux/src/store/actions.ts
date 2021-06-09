11e65f9776871daf4317dc6b0136df4875622fcb
kureharyosuke <ohhayo.com@gmail.com>
2021-06-09 23:28:12 +0900

    add: action  getTodos

--- React-Hook-Master/todo-redux/src/store/actions.ts
+++ React-Hook-Master/todo-redux/src/store/actions.ts
@@ -1,4 +1,7 @@
-import { Todo } from "./types";
+import { ThunkAction } from "redux-thunk";
+import { Action } from "redux";
+
+import { Todo, Store } from "./types";
 
 export const ADD_TODO = "ADD_TODO";
 export const DELETE_TODO = "DELETE_TODO";
@@ -50,3 +53,11 @@ export const setTodos = (todos: Todo[]): ActionTypes => ({
   type: SET_TODOS,
   payload: todos,
 });
+
+export const getTodos =
+  (url: string): ThunkAction<void, Store, unknown, Action<string>> =>
+  async (dispatch) => {
+    const resp = await fetch(url);
+    const todos: Todo[] = await resp.json();
+    dispatch(setTodos(todos));
+  };
