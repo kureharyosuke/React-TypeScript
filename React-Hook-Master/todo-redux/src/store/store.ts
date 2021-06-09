cb954157714a0cc2971ac2da9035a1b10bc99b87
kureharyosuke <ohhayo.com@gmail.com>
2021-06-09 22:42:35 +0900

    add: TodoAdd, TodoList => redux

--- React-Hook-Master/todo-redux/src/store/store.ts
+++ React-Hook-Master/todo-redux/src/store/store.ts
@@ -12,22 +12,22 @@ import { Store, Todo } from "./types";
 
 // Standard interface and functions
 
-export const updateTodo = (todos: Todo[], id: number, text: string): Todo[] =>
+const updateTodo = (todos: Todo[], id: number, text: string): Todo[] =>
   todos.map((todo) => ({
     ...todo,
     text: todo.id === id ? text : todo.text,
   }));
 
-export const toggleTodo = (todos: Todo[], id: number): Todo[] =>
+const toggleTodo = (todos: Todo[], id: number): Todo[] =>
   todos.map((todo) => ({
     ...todo,
     done: todo.id === id ? !todo.done : todo.done,
   }));
 
-export const removeTodo = (todos: Todo[], id: number): Todo[] =>
+const removeTodo = (todos: Todo[], id: number): Todo[] =>
   todos.filter((todo) => todo.id !== id);
 
-export const addTodo = (todos: Todo[], text: string): Todo[] => [
+const addTodo = (todos: Todo[], text: string): Todo[] => [
   ...todos,
   {
     id: Math.max(0, Math.max(...todos.map(({ id }) => id))) + 1,
