fa679b05bfbf359b43c7973506e4385e692f4154
kureharyosuke <ohhayo.com@gmail.com>
2021-06-16 22:24:50 +0900

    useCallback: [state]변경값과, if(timeout.current);

--- lecture2/ResponseCheck.tsx
+++ lecture2/ResponseCheck.tsx
@@ -5,9 +5,34 @@ const ResponseCheck = () => {
   const [state, setState] = useState<string | null>("waiting");
   const [message, setMessage] = useState<string | null>("클릭해서 시작하세요.");
   const [result, setResult] = useState<number[]>([]);
-  const timeout = useRef<number | null>(null);
-  const startTime = useRef<number | null>(0);
-  const endTime = useRef<number | null>(0);
+  const timeout = useRef<number>(null);
+  const startTime = useRef<number>(0);
+  const endTime = useRef<number>(0);
+
+  const onClickScreen = useCallback(() => {
+    if (state === 'waiting') {
+      timeout.current = setTimeout(() => { // 에러 날때, 제너릭을 꼭 확인!!
+        setState('now');
+        setMessage('지금 클릭');
+        startTime.current = new Date().getTime();
+      }, Math.floor(Math.random() * 1000) + 2000); // 2초~3초 랜덤
+      setState('ready');
+      setMessage('초록색이 되면 클릭하세요.');
+    } else if (state === 'ready') { // 성급하게 클릭
+      // clearTimeout(timeout.current!); #*** 이것보다는 if문으로 감싸서, 확실하게 에러를 차단해주는게 좋다.
+      if (timeout.current) {
+        clearTimeout(timeout.current)
+      }
+      setState('waiting');
+      setMessage('너무 성급하시군요! 초록색이 된 후에 클릭하세요.');
+    } else if (state === 'now') {
+      setState('waiting');
+      setMessage('클릭해서 시작하세요.');
+      setResult((prevResult) => {
+        return [...prevResult, endTime.current - startTime.current]
+      })
+    } // 반응속도 체크 
+  }, [state]) // state 가 조건문에 맞게 상태 값이 변경되니까, state를 넘어주어야 한다.
 
 
   const onReset = useCallback(() => {
