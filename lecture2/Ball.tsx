d6c9164a9c680641c29353453d7b29ff7e484812
kureharyosuke <ohhayo.com@gmail.com>
2021-06-17 23:54:19 +0900

    useMemo

---
+++ lecture2/Ball.tsx
@@ -0,0 +1,10 @@
+import { number } from 'prop-types'
+import React, { memo } from 'react'
+
+export const Ball = memo(({ number }: { number: number }) => {
+  let background;
+
+  if (number <= 10) {
+
+  }
+})
