18c1ab7128ee92148f0f0642f8fba2bd381ff281
kureharyosuke <ohhayo.com@gmail.com>
2021-07-04 03:41:57 +0900

    Install: @fower/react

--- React-Hook-Master/pokemon/src/App.tsx
+++ React-Hook-Master/pokemon/src/App.tsx
@@ -1,25 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Box } from "@fower/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box p-10 maxW-1200 m="auto">
+      <h1>Hello Pokemon</h1>
+      <Box as="input" p-5 text4XL border-1 roundedXL borderGray500 w="100%" />
+    </Box>
   );
 }
 
