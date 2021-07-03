9da8662076a746ddc36cf4f4c40db35085951f46
kureharyosuke <ohhayo.com@gmail.com>
2021-07-04 03:53:03 +0900

    evt: React.ChangeEvent<HTMLInputElement>

--- React-Hook-Master/pokemon/src/App.tsx
+++ React-Hook-Master/pokemon/src/App.tsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box } from "@fower/react";
+import { styled } from "@fower/styled";
+
+const Input = styled("input");
 
 function App() {
+  const [filter, setFilter] = useState("");
+
   return (
     <Box p-10 maxW-1200 m="auto">
       <h1>Hello Pokemon</h1>
-      <Box as="input" p-5 text4XL border-1 roundedXL borderGray500 w="100%" />
+      <Input
+        p-5
+        text4XL
+        border-1
+        roundedXL
+        borderGray500
+        w="100%"
+        value={filter}
+        onChange={(evt: React.ChangeEvent<HTMLInputElement>) =>
+          setFilter(evt.target.value)
+        }
+      />
+      {filter}
     </Box>
   );
 }
