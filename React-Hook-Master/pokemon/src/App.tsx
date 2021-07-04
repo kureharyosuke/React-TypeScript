b4b64ebfa27b15b60ee1937ca9237bd754904f6c
kureharyosuke <ohhayo.com@gmail.com>
2021-07-04 18:11:25 +0900

    import usePokemon => const {filter, setFilter, pokemon} = usePohemon(), useEffect(pokemon, change)

--- React-Hook-Master/pokemon/src/App.tsx
+++ React-Hook-Master/pokemon/src/App.tsx
@@ -2,36 +2,21 @@ import React, { useState, useEffect } from "react";
 import { Box } from "@fower/react";
 import { styled } from "@fower/styled";
 
-interface Pokemon {
-  id: number;
-  name: {
-    english: string;
-    japanese: string;
-  };
-  type: string[];
-  base: Record<string, number>;
-}
+import usePokemon from "./usePokemon";
 
 const Input = styled("input");
 
 function App() {
-  const [filter, setFilter] = useState<string>("");
-  const [allPokemon, setAllPokemon] = useState<Pokemon[]>([]);
+  const [count, setCount] = useState(1);
+  const { filter, setFilter, pokemon } = usePokemon();
 
   useEffect(() => {
-    fetch("/pokemon.json")
-      .then((resp) => resp.json())
-      .then((pokemon: Pokemon[]) => setAllPokemon(pokemon)); // setAllPokemon(pokemon) 파라미터를 감싸주고,
-  }, []);
-
-  // 검색바(Search Bar) filter로 english 이름만 필터하고,
-  const lcFilter = filter.toLowerCase();
-  const pokemon = allPokemon
-    .filter(({ name: { english } }) => english.toLowerCase().includes(lcFilter))
-    .slice(0, 10); // 검색수를 0번에서 10번까지
+    console.log("Pokemon changed");
+  }, [pokemon]);
 
   return (
     <Box p-10 maxW-1200 m="auto">
+      <button onClick={() => setCount(count + 1)}>Bump Count - {count}</button>
       <h1>Hello Pokemon</h1>
       <Input
         p-5
