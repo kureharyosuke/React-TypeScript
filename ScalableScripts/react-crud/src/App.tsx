bf45a649fabf131fb18c4ddaed713902058a88ea
kureharyosuke <ohhayo.com@gmail.com>
2021-08-15 22:33:12 +0900

    add: admin/Products - table

--- ScalableScripts/react-crud/src/App.tsx
+++ ScalableScripts/react-crud/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Nav from "./components/Nav";
 import SideMenu from "./components/SideMenu";
+import { Products } from "./admin/Products";
 
 function App() {
   return (
@@ -16,26 +17,7 @@ function App() {
       <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
         <h2>Section title</h2>
         <div className="table-responsive">
-          <table className="table table-striped table-sm">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">Header</th>
-                <th scope="col">Header</th>
-                <th scope="col">Header</th>
-                <th scope="col">Header</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1,001</td>
-                <td>random</td>
-                <td>data</td>
-                <td>placeholder</td>
-                <td>text</td>
-              </tr>
-            </tbody>
-          </table>
+          <Products />
         </div>
       </main>
     </div>
