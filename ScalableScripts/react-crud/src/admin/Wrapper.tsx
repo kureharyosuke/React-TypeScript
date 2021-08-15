22dc31442f735ac9dfc97aa237aaf01b860e7868
kureharyosuke <ohhayo.com@gmail.com>
2021-08-15 23:46:00 +0900

    type: PropsWithChildren -> React.ReactNode | undefined

--- ScalableScripts/react-crud/src/admin/Wrapper.tsx
+++ ScalableScripts/react-crud/src/admin/Wrapper.tsx
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { PropsWithChildren } from "react";
 import { Nav } from "../components/Nav";
 import { SideMenu } from "../components/SideMenu";
 
-export const Wrapper = () => {
+export const Wrapper = (props: PropsWithChildren<React.ReactNode>) => {
   return (
     <div>
       <Nav />
       <div className="container-fluid">
         <div className="row">
           <SideMenu />
-          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4"></main>
+          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+            {props.children}
+          </main>
         </div>
       </div>
     </div>
   );
 };
+
+// (alias) type PropsWithChildren<P> = P & {
+//     children?: ReactNode | undefined; // (alias) type ReactNode = ReactNode | null | undefined;
+// }
+// import PropsWithChildren
