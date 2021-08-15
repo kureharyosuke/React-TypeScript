0090587eae526ab7425aad40248e657aac567ad9
kureharyosuke <ohhayo.com@gmail.com>
2021-08-15 23:56:04 +0900

    fix: clean component

--- ScalableScripts/react-crud/src/App.tsx
+++ ScalableScripts/react-crud/src/App.tsx
@@ -1,26 +1,16 @@
 import React from "react";
-import { BrowserRouter, Route, Router } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
-import { Nav } from "./components/Nav";
-import { SideMenu } from "./components/SideMenu";
 import { Products } from "./admin/Products";
 import { Main } from "./main/Main";
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <div className="container-fluid">
-        <div className="row">
-          <SideMenu />
-          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            <BrowserRouter>
-              <Route path="/" component={Main} />
-              <Route path="/admin/products" component={Products} />
-            </BrowserRouter>
-          </main>
-        </div>
-      </div>
+      <BrowserRouter>
+        <Route path="/" exact component={Main} />
+        <Route path="/admin/products" component={Products} />
+      </BrowserRouter>
     </div>
   );
 }
--- ScalableScripts/react-crud/src/admin/Products.tsx
+++ ScalableScripts/react-crud/src/admin/Products.tsx
@@ -1,28 +1,34 @@
 import React from "react";
+import { Wrapper } from "./Wrapper";
 
 export const Products = () => {
   return (
-    <div className="table-responsive">
-      <table className="table table-striped table-sm">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Header</th>
-            <th scope="col">Header</th>
-            <th scope="col">Header</th>
-            <th scope="col">Header</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1,001</td>
-            <td>random</td>
-            <td>data</td>
-            <td>placeholder</td>
-            <td>text</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <Wrapper>
+      <div className="table-responsive">
+        <table className="table table-striped table-sm">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Header</th>
+              <th scope="col">Header</th>
+              <th scope="col">Header</th>
+              <th scope="col">Header</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1,001</td>
+              <td>random</td>
+              <td>data</td>
+              <td>placeholder</td>
+              <td>text</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </Wrapper>
   );
 };
+
+// (alias) const Wrapper: (props: PropsWithChildren<React.ReactNode>) => JSX.Element
+// import Wrapper
